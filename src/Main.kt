1c0109f598c015e1f8ef467be04237007947c5e6
Caren <caren.arevalo-v@uniminuto.edu.co>
2025-02-14 13:42:55 -0500

    Salidas por consola

--- src/Main.kt
+++ src/Main.kt
@@ -3,5 +3,12 @@
 fun main() {
    println("Hola Mundo")
     println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
+    println("Primer Ejercicio Kotlin")
 
 }
